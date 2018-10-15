fefa55d1911d4726d9a8aff4f68b436cd2c19e70
Anton Kanopkin <kanstudio@mail.ru>
2018-10-15 13:01:36 +0300

    Fix

---
+++ __tests__/ex6_js-array-methods.js
@@ -0,0 +1,11 @@
+const task = require('~utils').createTask('ex6_js-array-methods');
+
+describe('Ex6. JS Array methods', () => {
+    task('01', code => {
+        const arr = ['a', 'b', 'c', 'd', 'e'];
+
+        it("should copy from element 1 and to element 4 (without including)", () => {
+            expect(code(arr, 1, 4).toBe(['e', 'c', 'd']));
+        });
+    });
+});
